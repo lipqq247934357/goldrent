6d8bd10d19ea271cd9d8a631b3b6ad9457cc60f9
lipeng <247934357@qq.com>
2019-04-11 11:48:56 +0800

    修改登录页面

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,48 +1,54 @@
 <template>
-<div class="login">
-    <img src="./logo.png" alt="">
-    <div class="inputText">
-        <div class="subTextdiv">
-            <span>登录名：</span>
-            <input type="text" name="" value="">
-        </div>
-        <div class="subTextdiv">
-            <span class="passwordText">密&nbsp;&nbsp;&nbsp;码：</span>
-            <input type="password" name="" value="">
+    <div class="login">
+        <img src="./logo.png" alt="">
+        <div class="inputText">
+            <div class="subTextdiv">
+                <span>登录名：</span>
+                <input type="text" name="" value="">
+            </div>
+            <div class="subTextdiv">
+                <span class="passwordText">密&nbsp;&nbsp;&nbsp;码：</span>
+                <input type="password" name="" value="">
+            </div>
         </div>
     </div>
-</div>
 </template>
 
-<script  type="text/ecmascript-6">
-export default {
-
-}
+<script type="text/ecmascript-6">
+    export default {}
 </script>
 <style lang="less">
-.login {
-    font-size: 20px;
-    background: #ffe3c7;
-    height: calc(100vh - 50px);
-    img {
-        margin: 100px auto 50px;
-        display: block;
-    }
-    .inputText {
-        width: 300px;
-        padding:  10px;
-        margin: 0 auto;
-        .subTextdiv {
-            height: 50px;
-            line-height: 30px;
-            .passwordText {
-            }
-            input {
-                width: 200px;
-                height: 30px;
-                padding-left: 10px;
+    .login {
+        font-size: 20px;
+        background: #ffe3c7;
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        img {
+            margin: 15% auto 50px;
+            display: block;
+        }
+
+        .inputText {
+            width: 300px;
+            padding: 10px;
+            margin: 0 auto;
+
+            .subTextdiv {
+                height: 50px;
+                line-height: 30px;
+
+                .passwordText {
+                }
+
+                input {
+                    width: 200px;
+                    height: 30px;
+                    padding-left: 10px;
+                }
             }
         }
     }
-}
 </style>
