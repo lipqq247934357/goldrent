23572474a359b3ac097bde21fa11c3f62f80a829
daidai <349867227@qq.com>
2019-04-11 09:42:38 +0800

    静态页面

--- src/components/loanapproval/operation.vue
+++ src/components/loanapproval/operation.vue
@@ -38,7 +38,7 @@
             </li>
         </ul>
         <h3>累计得分：88 建议通过</h3>
-        <table style="width: 100%" border>
+        <table style="width: 100%" border="1">
             <tr>
                 <th>指标大类</th>
                 <th>指标名称</th>
@@ -46,40 +46,131 @@
                 <th>得分</th>
             </tr>
             <tr>
-                <td rowspan="4">基本情况</td>
-            </tr>
-            <tr>
+                <td rowspan="3">基本情况</td>
                 <td>客户类别</td>
+                <td>年龄段</td>
+                <td>X</td>
             </tr>
             <tr>
                 <td>婚姻状况</td>
+                <td>婚姻状况</td>
+                <td>X</td>
             </tr>
             <tr>
                 <td>年龄段</td>
+                <td>年龄段</td>
+                <td>X</td>
             </tr>
             <tr>
-                <td rowspan="4">资产状况</td>
+                <td rowspan="3">资产状况</td>
+                <td>客户类别</td>
+                <td>年龄段</td>
+                <td>X</td>
             </tr>
             <tr>
-                <td>房产</td>
+                <td>婚姻状况</td>
+                <td>婚姻状况</td>
+                <td>X</td>
             </tr>
             <tr>
-                <td>农机具，机动车</td>
+                <td>年龄段</td>
+                <td>年龄段</td>
+                <td>X</td>
             </tr>
             <tr>
-                <td>收入偿债比</td>
+                <td rowspan="3">征信状况</td>
+                <td>客户类别</td>
+                <td>年龄段</td>
+                <td>X</td>
             </tr>
             <tr>
-                <td rowspan="4">征信状况</td>
+                <td>婚姻状况</td>
+                <td>婚姻状况</td>
+                <td>X</td>
             </tr>
             <tr>
-                <td>信用卡及贷款24个月最大逾期期数</td>
+                <td>年龄段</td>
+                <td>年龄段</td>
+                <td>X</td>
             </tr>
             <tr>
-                <td>信用卡及贷款24个月累计逾期期数</td>
+                <td rowspan="4">保证人</td>
+                <td>客户类别</td>
+                <td>年龄段</td>
+                <td>X</td>
             </tr>
             <tr>
-                <td>最近一个月内的查询机构数（贷款审批）</td>
+                <td>婚姻状况</td>
+                <td>婚姻状况</td>
+                <td>X</td>
+            </tr>
+            <tr>
+                <td>年龄段</td>
+                <td>年龄段</td>
+                <td>X</td>
+            </tr>
+            <tr>
+                <td>年龄段</td>
+                <td>年龄段</td>
+                <td>X</td>
+            </tr>
+            <tr>
+                <td rowspan="3">回购人</td>
+                <td>客户类别</td>
+                <td>年龄段</td>
+                <td>X</td>
+            </tr>
+            <tr>
+                <td>婚姻状况</td>
+                <td>婚姻状况</td>
+                <td>X</td>
+            </tr>
+            <tr>
+                <td>年龄段</td>
+                <td>年龄段</td>
+                <td>X</td>
+            </tr>
+            <tr>
+                <td>首付比例</td>
+                <td></td>
+                <td>XXXXXXXXXXXXXX</td>
+                <td>XXXXXX</td>
+            </tr>
+            <tr>
+                <td>农机补贴</td>
+                <td></td>
+                <td>XXXXXXXXXXXXXX</td>
+                <td>XXXXXX</td>
+            </tr>
+            <tr>
+                <td>贷款记录</td>
+                <td></td>
+                <td>XXXXXXXXXXXXXX</td>
+                <td>XXXXXX</td>
+            </tr>
+            <tr>
+                <td>对外担保</td>
+                <td></td>
+                <td>XXXXXXXXXXXXXX</td>
+                <td>XXXXXX</td>
+            </tr>
+            <tr>
+                <td>婚姻状况关注项</td>
+                <td></td>
+                <td>XXXXXXXXXXXXXX</td>
+                <td>XXXXXX</td>
+            </tr>
+            <tr>
+                <td>涉诉</td>
+                <td></td>
+                <td>XXXXXXXXXXXXXX</td>
+                <td>XXXXXX</td>
+            </tr>
+            <tr>
+                <td>被执行信息</td>
+                <td></td>
+                <td>XXXXXXXXXXXXXX</td>
+                <td>XXXXXX</td>
             </tr>
         </table>
     </div>
@@ -133,6 +224,15 @@ export default {
                 padding-left: 10px;
             }
         }
+    }
+}
+table {
+    text-align: center;
+    border: 1px solid #EBEEF5;
+    color: #909399;
+    tr {
+        height: 50px;
+        line-height: 50px;
     }
 }
 </style>
