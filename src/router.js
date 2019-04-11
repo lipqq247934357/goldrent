e6ea3ee55d4a92b83243d7a0bf7030f7e03ea6f7
lipeng <247934357@qq.com>
2019-04-11 10:04:37 +0800

    修改文件名错误

--- src/router.js
+++ src/router.js
@@ -4,9 +4,9 @@ import welcome from './views/index/index.vue';
 import login from './views/login/login.vue'; // 登陆页面
 import loanapprovallist from './views/loanapproval/index.vue'; //贷款审批列表
 import loadapproval from './views/loanapproval/loanApprovalDetail.vue'; // 贷款审批
-import businfo from './views/businessinfo/businessInfo.vue'; // 业务信息列表
+import businfo from './views/businessInfo/businessInfo.vue'; // 业务信息列表
 import businessadmin from './views/businessM/index.vue'; //商业伙伴管理
-import businessdetail from './views/businessinfo/businessDetail.vue'; // 业务信息查看
+import businessdetail from './views/businessInfo/businessDetail.vue'; // 业务信息查看
 import addbusine from './views/businessM/addbusine.vue'; //添加商业伙伴
 import legalperson from './views/businessM/legalperson.vue'; //法人
 import naturalperson from './views/businessM/naturalperson.vue'; //自然人
@@ -17,55 +17,55 @@ export default new Router({
     base: process.env.BASE_URL,
     routes: [
         {
-          path: '/',
-          name: 'welcome',
-          component: welcome
+            path: '/',
+            name: 'welcome',
+            component: welcome
         },
         {
-          path: '/login',
-          name: 'login',
-          component: login
+            path: '/login',
+            name: 'login',
+            component: login
         },
         {
-          path: '/loanapprovallist',
-          name: 'loanapprovallist',
-          component: loanapprovallist
-       },
-       {
-         path: '/loadapproval',
-         name: 'loadapproval',
-         component: loadapproval
-       },
-       {
-        path: '/businfo',
-        name: 'businfo',
-        component: businfo
+            path: '/loanapprovallist',
+            name: 'loanapprovallist',
+            component: loanapprovallist
         },
         {
-         path: '/businessdetail',
-         name: 'businessdetail',
-         component: businessdetail
+            path: '/loadapproval',
+            name: 'loadapproval',
+            component: loadapproval
         },
         {
-         path: '/addbusine',
-         name: 'addbusine',
-         component: addbusine
+            path: '/businfo',
+            name: 'businfo',
+            component: businfo
         },
         {
-         path: '/businessM',
-         name: 'businessadmin',
-         component: businessadmin
-         },
-         {
-          path: '/legalperson',
-          name: 'legalperson',
-          component: legalperson
-          },
-          {
-           path: '/naturalperson',
-           name: 'naturalperson',
-           component: naturalperson
-           },
+            path: '/businessdetail',
+            name: 'businessdetail',
+            component: businessdetail
+        },
+        {
+            path: '/addbusine',
+            name: 'addbusine',
+            component: addbusine
+        },
+        {
+            path: '/businessM',
+            name: 'businessadmin',
+            component: businessadmin
+        },
+        {
+            path: '/legalperson',
+            name: 'legalperson',
+            component: legalperson
+        },
+        {
+            path: '/naturalperson',
+            name: 'naturalperson',
+            component: naturalperson
+        },
 
         // {
         //   path: '/about',
