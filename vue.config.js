ed1dac8e3da8e12d83e55cf768076794293db8ba
daidai <349867227@qq.com>
2019-04-18 10:02:27 +0800

    冲突

--- vue.config.js
+++ vue.config.js
@@ -4,19 +4,11 @@ module.exports = {
     devServer: {
         proxy: {
             '/web': {
-<<<<<<< HEAD
-                target: 'http://172.16.32.176:8081/',
-                ws: false,
-                changeOrigin: true,
-                pathRewrite: {
-                     // '^/web': ''
-=======
                 target: 'http://172.16.32.206:8081/',
                 ws: false,
                 changeOrigin: true,
                 pathRewrite: {
                     // '^/api': ''
->>>>>>> 164ba4d893b2030375b8f09557116002f8a73713
                 }
             }
         }
