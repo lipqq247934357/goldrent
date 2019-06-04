b9a2f1c31b664d421d616039c77570eb0284079d
lipeng <247934357@qq.com>
2019-06-04 17:26:32 +0800

    修改贷款审批操作距离左边距离太宽

--- src/components/approvalopinions/approvalopinions.vue
+++ src/components/approvalopinions/approvalopinions.vue
@@ -249,7 +249,7 @@ export default {
                     bussNo: this.$route.query.bussNo
                 }).then(res => {
                     if(JSON.stringify(res.data.data)) {
-``                        if(JSON.stringify(res.data.data.taskType20)) {
+                        if(JSON.stringify(res.data.data.taskType20)) {
                             this.disabledtextarea = res.data.data.taskType20.reasonDescription;
                         }
                         if(JSON.stringify(res.data.data.taskType23)) {
