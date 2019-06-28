3e437628d76b34e72cf406a7e1ad9cd848e6c826
lipeng <247934357@qq.com>
2019-06-28 16:59:16 +0800

    修改业务信息租赁要素展示内容bug

--- src/views/loanapproval/item/wantfactor.vue
+++ src/views/loanapproval/item/wantfactor.vue
@@ -169,7 +169,7 @@
             }
         },
         created() {
-            let arrangementtype = this.$route.query.arrangement;
+            let arrangementtype = this.$route.query.arrangementtype;
             if (arrangementtype != 5 && arrangementtype != 6) {
                 this.taskType = '10'
             } else {
